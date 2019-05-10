c2ff16a3acd543d7ec290d366549e705740587d2
Marios <touloupos@gmail.com>
2019-05-10 14:03:27 +0300

    Strict Slashes set to false

--- src/rec_sys/main.py
+++ src/rec_sys/main.py
@@ -37,9 +37,8 @@ from rec_methods import methods
 import json_logging, logging, sys
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-blueprint = Blueprint('api', __name__, url_prefix='tng-vnv-dsm/api/v1')
+blueprint = Blueprint('api', __name__, url_prefix='/tng-vnv-dsm/api/v1')
 api = Api(blueprint, version="0.1",
           title='5GTANGO tng-vnv-dsm API',
           description="5GTANGO Decision Support Mechanism - Test Recommendation System.")
